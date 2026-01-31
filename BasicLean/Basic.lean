37818f1bf42d914c86c626c7c656d151aeacf229
Kaue Silveira <silveira.kaue@gmail.com>
2026-01-31 12:29:56 +0100

    simplify congr proofs

--- BasicLean/Basic.lean
+++ BasicLean/Basic.lean
@@ -45,18 +45,15 @@ theorem Eq.symm {α : Sort u} {a b : α} (h_Eq_a_b : Eq a b) : Eq b a :=
   Eq.subst (motive := fun x => Eq x a) h_Eq_a_b h_Eq_a_a
 
 theorem Eq.trans {α : Sort u} {a b c : α} (h_Eq_a_b : Eq a b) (h_Eq_b_c : Eq b c) : Eq a c :=
-  have h_Eq_b_a : Eq b a := Eq.symm h_Eq_a_b
-  Eq.subst (motive := fun x => Eq x c) h_Eq_b_a h_Eq_b_c
+  Eq.subst (motive := fun x => Eq a x) h_Eq_b_c h_Eq_a_b
 
 theorem congrArg {α : Sort u} {β : Sort v} {a₁ a₂ : α} (f : α → β) (h_Eq_a1_a2: Eq a₁ a₂) : Eq (f a₁) (f a₂) :=
-  have h_Eq_a2_a1 : Eq a₂ a₁ := Eq.symm h_Eq_a1_a2
-  have h_Eq_f_a2_f_a2: Eq (f a₂) (f a₂) := Eq.refl (f a₂)
-  Eq.subst (motive := fun x => Eq (f x) (f a₂)) h_Eq_a2_a1 h_Eq_f_a2_f_a2
+  have h_Eq_f_a1_f_a1: Eq (f a₁) (f a₁) := Eq.refl (f a₁)
+  Eq.subst (motive := fun x => Eq (f a₁) (f x)) h_Eq_a1_a2 h_Eq_f_a1_f_a1
 
 theorem congrFun {α : Sort u} {β : α → Sort v} {f g : (x : α) → β x} (h_Eq_f_g : Eq f g) (a : α) : Eq (f a) (g a) :=
-  have h_Eq_g_f : Eq g f := Eq.symm h_Eq_f_g
-  have h_Eq_g_a_g_a : Eq (g a) (g a) := Eq.refl (g a)
-  Eq.subst (motive := fun x => Eq (x a) (g a)) h_Eq_g_f h_Eq_g_a_g_a
+  have h_Eq_f_a_f_a : Eq (f a) (f a) := Eq.refl (f a)
+  Eq.subst (motive := fun x => Eq (f a) (x a)) h_Eq_f_g h_Eq_f_a_f_a
 
 theorem congr {α : Sort u} {β : Sort v} {f₁ f₂ : α → β} {a₁ a₂ : α} (h_Eq_f1_f2 : Eq f₁ f₂) (h_Eq_a1_a2 : Eq a₁ a₂) : Eq (f₁ a₁) (f₂ a₂) :=
   have h_Eq_f1_a1_f1_a2 : Eq (f₁ a₁) (f₁ a₂) := congrArg f₁ h_Eq_a1_a2
