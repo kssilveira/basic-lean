7963d59ee5b99378051a1507745cbe229fa806a4
Kaue Silveira <silveira.kaue@gmail.com>
2026-01-31 14:05:52 +0100

    Or

--- BasicLean/Basic.lean
+++ BasicLean/Basic.lean
@@ -6,8 +6,11 @@ inductive True : Prop where
   | intro : True
 
 example : True := True.intro
+example : a → True := fun _ => True.intro
 
 inductive False : Prop
+
+example : False → False := fun x => x
 
 def False.elim {b : Sort u} (false : False) : b :=
   false.rec
@@ -17,6 +20,8 @@ example (false : False) : b := false.elim
 
 def Not (a : Prop) : Prop := a → False
 
+example : Not False := fun x => x
+
 def absurd {a : Prop} {b : Sort v} (ha : a) (Not_a : Not a) : b :=
   have false : False := Not_a ha
   have res : b := false.elim
@@ -24,14 +29,12 @@ def absurd {a : Prop} {b : Sort v} (ha : a) (Not_a : Not a) : b :=
 
 example (ha : a) (Not_a : Not a) : b := absurd ha Not_a
 
-def id {α : Sort u} (a : α) : α := a
-
-example : Not False := id
-
 inductive Eq : α → α → Prop where
   | refl (a : α) : Eq a a
 
 example : Eq a a := Eq.refl a
+
+def id {α : Sort u} (a : α) : α := a
 
 theorem id_eq (a : α) : Eq (id a) a := Eq.refl a
 
@@ -81,12 +84,45 @@ structure And (a b : Prop) : Prop where
   left : a
   right : b
 
+example : And True True := And.intro True.intro True.intro
 example (ha : a) : And a a := And.intro ha ha
 example (ha : a) (hb : b) : And a b := And.intro ha hb
 example (And_ab : And a b) : a := And_ab.left
 example (And_ab : And a b) : b := And_ab.right
 
-theorem And.symmetric (And_ab: And a b) : And b a :=
+theorem And.symm (And_ab: And a b) : And b a :=
   have ha : a := And_ab.left
   have hb : b := And_ab.right
   And.intro hb ha
+
+inductive Or (a b : Prop) : Prop where
+  | inl (h : a) : Or a b
+  | inr (h : b) : Or a b
+
+theorem Or.intro_left (b : Prop) (h : a) : Or a b :=
+  Or.inl h
+
+theorem Or.intro_right (a : Prop) (h : b) : Or a b :=
+  Or.inr h
+
+example : Or True b := Or.intro_left b True.intro
+example : Or b True := Or.intro_right b True.intro
+example (ha : a) : Or a b := Or.intro_left b ha
+example (ha : a) : Or b a := Or.intro_right b ha
+
+theorem Or.elim {c : Prop} (Or_ab : Or a b) (left : a → c) (right : b → c) : c :=
+  match Or_ab with
+  | Or.inl h => left h
+  | Or.inr h => right h
+
+theorem Or.resolve_left  (Or_ab : Or a b) (Not_a : Not a) : b :=
+  Or_ab.elim (fun ha => absurd ha Not_a) id
+
+theorem Or.resolve_right (Or_ab: Or a b) (Not_b : Not b) : a :=
+  Or_ab.elim id (fun hb => absurd hb Not_b)
+
+theorem Or.neg_resolve_left (Or_Not_a_b : Or (Not a) b) (ha : a) : b :=
+  Or_Not_a_b.elim (fun Not_a => absurd ha Not_a) id
+
+theorem Or.neg_resolve_right (Or_a_Not_b : Or a (Not b)) (hb : b) : a :=
+  Or_a_Not_b.elim id (fun Not_b => absurd hb Not_b)
